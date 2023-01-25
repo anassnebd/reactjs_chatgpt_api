b6b1f3a3c482f41934a82c6d0c4145e8aa6d603d
AnassNebdaoui <nebdaoui.anass@gmail.com>
2023-01-25 19:50:15 +0100

    TailwindCss

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='text-red-800'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="text-xs text-green-600"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
