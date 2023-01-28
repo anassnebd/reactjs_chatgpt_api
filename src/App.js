048f6665260757b0dd88cbcaa0e71d0a79681113
AnassNebdaoui <nebdaoui.anass@gmail.com>
2023-01-28 17:43:08 +0100

    axios

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import { Configuration, OpenAIApi } from 'openai';
+import axios from 'axios';
 
 function App() {
   const configuration = new Configuration({
