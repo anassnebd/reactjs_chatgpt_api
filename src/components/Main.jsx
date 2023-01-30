d41ad567babb0b22b7528875a2a8339961a5c8bd
AnassNebdaoui <nebdaoui.anass@gmail.com>
2023-01-30 20:31:41 +0100

    Loading + api chatgpt on main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,54 @@
-import React from 'react'
-import './components.scss'
+import React, { useState } from 'react';
+import { Configuration, OpenAIApi } from 'openai';import './components.scss'
 import SendIcon from '../assets/icons/send.png'
+import ReactLoading from 'react-loading';
+
 
 function Main() {
+    const configuration = new Configuration({
+        apiKey: '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',
+      });
+    
+      const openai = new OpenAIApi(configuration);
+    
+      const [prompt, setPrompt] = useState('');
+      const [result, setResult] = useState('');
+      const [loading, setLoading] = useState(false);
+    
+      const handleClick = async () => {
+        setLoading(true);
+        try {
+          const response = await openai.createCompletion({
+            model: 'text-davinci-002',
+            prompt: prompt,
+            temperature: 0.5,
+            max_tokens: 100,
+          });
+          setResult(response.data.choices[0].text);
+        } catch (error) {
+          console.log(error);
+        }
+        setLoading(false);
+      };
+    
+
   return (
     <div className='relative main h-screen w-10/12 block'>
         <div className='h-5 w-10 bg-red-500'>
-
+            <p>jj</p>
         </div>
         <div className='absolute w-3/6 bottom-12 right-0 left-0 center flex'>
-            <input type="text" name="" id="" className='w-11/12 h-10 bg-input-primary rounded-l-sm outline-none pl-5 text-sm'/>
-            <button className='bg-zinc-700 text-black w-1/12 rounded-r-sm outline-transparent'><img src={SendIcon} className='w-4 center' alt="" /></button>
+            <input 
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+            placeholder="Enter a prompt here"
+            type="text" name="" id="" className='w-11/12 h-10 bg-input-primary rounded-l-sm outline-none pl-5 text-sm'/>
+            <button 
+            onClick={handleClick} disabled={loading || prompt.length === 0}
+            className='bg-zinc-700 text-black w-1/12 rounded-r-sm outline-transparent'>
+                {loading ? <ReactLoading type={'spin'} color={'#ffffff'} height={'25%'} width={'25%'} className='center'/> : <img src={SendIcon} className='w-4 center' alt="" />}
+                {/* <img src={SendIcon} className='w-4 center' alt="" /> */}
+            </button>
 
         </div>  
     </div>
